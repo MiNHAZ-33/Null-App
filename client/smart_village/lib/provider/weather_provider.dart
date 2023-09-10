fe5a35c8988ac2d48125f0ec6db795ef8396bde6
MiNHAZ-33 <miminhaz33@gmail.com>
2023-09-10 21:34:13 +0600

    added api endpoint for weather

--- client/smart_village/lib/provider/weather_provider.dart
+++ client/smart_village/lib/provider/weather_provider.dart
@@ -1,9 +1,15 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
+
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class WeatherProvider extends ChangeNotifier {
+  double lat = 24.2299;
+  double lon = 89.7122;
+  static const String weatherApi = 'http://localhost:5124/api/Weather';
+
   Weather _weather = Weather(
     tempC: 0.0,
     humidity: 0.0,
@@ -11,9 +17,21 @@ class WeatherProvider extends ChangeNotifier {
   );
   Weather get weather => _weather;
 
-  void updateWeather(Weather weather) {
-    _weather = weather;
-    notifyListeners();
+  Future<void> updateWeather() async {
+    try {
+      final response =
+          await http.get(Uri.parse('$weatherApi?lat=${lat}&${lon}'));
+      if (response.statusCode == 200) {
+        Weather weather = Weather.fromJson(response.body);
+        _weather = weather;
+        print(weather.toString());
+        notifyListeners();
+      } else {
+        print('Request failed with status: ${response.statusCode}.');
+      }
+    } catch (e) {
+      print(e);
+    }
   }
 }
 
@@ -57,19 +75,20 @@ class Weather {
 
   String toJson() => json.encode(toMap());
 
-  factory Weather.fromJson(String source) => Weather.fromMap(json.decode(source) as Map<String, dynamic>);
+  factory Weather.fromJson(String source) =>
+      Weather.fromMap(json.decode(source) as Map<String, dynamic>);
 
   @override
-  String toString() => 'Weather(tempC: $tempC, humidity: $humidity, condition: $condition)';
+  String toString() =>
+      'Weather(tempC: $tempC, humidity: $humidity, condition: $condition)';
 
   @override
   bool operator ==(covariant Weather other) {
     if (identical(this, other)) return true;
-  
-    return 
-      other.tempC == tempC &&
-      other.humidity == humidity &&
-      other.condition == condition;
+
+    return other.tempC == tempC &&
+        other.humidity == humidity &&
+        other.condition == condition;
   }
 
   @override
